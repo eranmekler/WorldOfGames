1387bc9ac56a3b3d34a20035b9c1f37708f53a6a
eran mekler <meklereran@gmail.com>
2022-03-09 00:33:08 +0200

    selenium issues

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC

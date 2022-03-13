31e2de306aa624305c53103369dfd8552f154296
eran mekler <meklereran@gmail.com>
2022-03-13 21:02:57 +0200

    no msg

--- tests/e2e.py
+++ tests/e2e.py
@@ -8,7 +8,7 @@ from time import sleep
 def test_scores_service(app_url):
     chrome_options = webdriver.ChromeOptions()
     driver = webdriver.Remote(
-        command_executor='http://docker:4444/wd/hub',
+        command_executor='http://localhost:4444/wd/hub',
         options=chrome_options
     )
 
